40d9bdeb7aad8a814baaac4c37446aeb66da5f07
ChainsFox <shykid4242@gmail.com>
2025-02-07 01:01:00 +0700

    p4 done

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision objectWeHit)
     {
-        if (collision.gameObject.CompareTag("Target"))
+        if (objectWeHit.gameObject.CompareTag("Target"))
         {
-            print("hit " + collision.gameObject.name + "!");
+            print("hit " + objectWeHit.gameObject.name + "!");
+            createBulletImpactEffect(objectWeHit);
             Destroy(gameObject);
         }
 
-        if (collision.gameObject.CompareTag("Wall"))
+        if (objectWeHit.gameObject.CompareTag("Wall"))
         {
             print("hit a wall!");
+            createBulletImpactEffect(objectWeHit);
             Destroy(gameObject);
         }
     }
+
+    void createBulletImpactEffect(Collision objectWeHit)
+    {
+        ContactPoint contact = objectWeHit.contacts[0]; //first point that our bullet will hit
+
+        GameObject hole = Instantiate(GlobalReferences.Instance.bulletImpactEffectPrefab, //the thing we want to instantiate
+            contact.point, //position we instantiate it at 
+            Quaternion.LookRotation(contact.normal)); //rotation that we hit the actual target
+
+        hole.transform.SetParent(objectWeHit.gameObject.transform); //make the thing that we hit a parent of this bullet hole effect
+    }
+
 }
---
+++ Assets/Scripts/GlobalReferences.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalReferences : MonoBehaviour
+{
+    public static GlobalReferences Instance { get; set; }
+
+    public GameObject bulletImpactEffectPrefab;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this) //if the instance is not null and is not this one then we destroy it
+        {
+            Destroy(gameObject); //we only want 1 instance of this global references all of the time
+        }
+        else
+        {
+            Instance = this; //this is also the usual way we create a singleton
+        }
+    }
+
+
+}
