47530b7cdf5713f8c8ed7a8e7136b6045819502f
ChainsFox <shykid4242@gmail.com>
2025-07-22 18:36:29 +0700

    711

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,22 +21,40 @@ public class PlayerMovement : MonoBehaviour
     bool isMoving;
 
     private Vector3 lastPosition = new Vector3(0f,0f,0f);
-    // Start is called before the first frame update
+
+    //GrapplingHook 
+    public bool freeze;
+    public bool activeGrapple;
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (activeGrapple) return; //grapple freeze
+
         //ground check 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask); //create a sphere below the player with groundDistance height that will check if we touch the ground
         
         //resetting the default velocity
-        if(isGrounded && velocity.y < 0 )
+        if(isGrounded && velocity.y < 0 && !activeGrapple)
         {
             velocity.y = -2f; //if we jump and land back on the ground, it will reset the velocity
+        }
+
+        //grappling
+        if (freeze)
+        {
+            speed = 0f;
+            velocity = Vector3.zero;
+        }
+        else
+        {
+            speed = 12f;
         }
 
         //getting the inputs
@@ -75,4 +93,40 @@ public class PlayerMovement : MonoBehaviour
     
     
     }
+    //GrapplingHook:
+    public void JumpToPosition(Vector3 targetPosition, float trajectoryHeight)
+    {
+        activeGrapple = true;
+
+        velocityToSet = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+        Invoke(nameof(SetVelocity), 0.1f);
+
+        //Invoke(nameof(ResetRestrictions), 3f);
+
+        velocity = CalculateJumpVelocity(transform.position, targetPosition, trajectoryHeight);
+    }
+
+    private Vector3 velocityToSet;
+    private void SetVelocity()
+    {
+        //enableMovementOnNextTouch = true;
+        //rb.velocity = velocityToSet;
+        velocity = velocityToSet;
+
+        //cam.DoFov(grappleFov);
+    }
+
+    public Vector3 CalculateJumpVelocity(Vector3 startPoint, Vector3 endPoint, float trajectoryHeight)
+    {
+        float gravity = Physics.gravity.y;
+        float displacementY = endPoint.y - startPoint.y;
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
+
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
+            + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
+
+        return velocityXZ + velocityY;
+    }
+
 }
